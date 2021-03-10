727b3470a5e77667ab0944c4f56932d8ed5a52b4
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-03-09 21:50:20 -0600

    author con verificacion de roles

--- server/src/components/author/controller.ts
+++ server/src/components/author/controller.ts
@@ -1,54 +1,45 @@
 import { Request, Response } from "express";
-import { IPaginatedRequest } from "../types";
 import { errorResponse } from "../util";
 import { Author } from "./model";
 
 export async function createAuthor(req: Request, res: Response) {
-    try {
-        const { name } = req.body;
-        await Author.create({ name }).save();
-        res.send({ error: false });
-    } catch (error) {
-        res.send(errorResponse(error));
-    }
+  try {
+    const { name } = req.body;
+    await Author.create({ name }).save();
+    res.send({ error: false });
+  } catch (error) {
+    res.send(errorResponse(error));
+  }
 }
 
 export async function getAuthor(req: Request, res: Response) {
-    try {
-        const { id } = req.params;
-        const author = await Author.findOneOrFail({ where: { id } });
-        res.send(author);
-    } catch (error) {
-        res.send(errorResponse(error));
-    }
-}
-
-export async function getAuthors(req: IPaginatedRequest, res: Response) {
-    try {
-        res.send(req.paginated);
-    } catch (error) {
-        res.send(errorResponse(error));
-    }
+  try {
+    const { id } = req.params;
+    const author = await Author.findOneOrFail({ where: { id } });
+    res.send(author);
+  } catch (error) {
+    res.send(errorResponse(error));
+  }
 }
 
 export async function deleteAuthor(req: Request, res: Response) {
-    try {
-        const { id } = req.params;
-        await Author.delete(id);
-        res.send({ okay: true });
-    } catch (error) {
-        res.send(errorResponse(error));
-    }
+  try {
+    const { id } = req.params;
+    await Author.delete(id);
+    res.send({ okay: true });
+  } catch (error) {
+    res.send(errorResponse(error));
+  }
 }
 
 export async function updateAuthor(req: Request, res: Response) {
-    try {
-        const { id } = req.params;
-        const body = req.body;
-        const author = await Author.findOne(id);
-        await Author.save({ ...author, ...body });
-        res.send({ okay: true });
-    } catch (error) {
-        res.send(errorResponse(error));
-    }
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const author = await Author.findOne(id);
+    await Author.save({ ...author, ...body });
+    res.send({ okay: true });
+  } catch (error) {
+    res.send(errorResponse(error));
+  }
 }
--- server/src/components/author/routes.ts
+++ server/src/components/author/routes.ts
@@ -1,8 +1,26 @@
 import { Router } from "express";
-import { paginate } from "../util/pagination";
-import { getAuthors } from "./controller";
+import { authorizationMiddleware } from "../../middlewares";
+import { paginate, sendPaginatedData } from "../../middlewares/pagination";
+import { canUse } from "../util/permission/middleware";
+import {
+  createAuthor,
+  deleteAuthor,
+  getAuthor,
+  updateAuthor,
+} from "./controller";
 import { Author } from "./model";
 
 export const authorRoutes = Router();
 
-authorRoutes.route("/test").get(paginate(Author), getAuthors);
+authorRoutes.use(authorizationMiddleware, canUse("admin"));
+
+authorRoutes
+  .route("/")
+  .get(paginate(Author), sendPaginatedData)
+  .post(createAuthor);
+
+authorRoutes
+  .route("/:id")
+  .get(getAuthor)
+  .delete(deleteAuthor)
+  .put(updateAuthor);
