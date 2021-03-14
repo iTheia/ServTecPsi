e7bb2992a2f9000abdd268c232f6d69ea1a5b540
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-03-13 22:24:20 -0600

    arq hex

--- server/src/components/daily_phrase/util/index.ts
+++
@@ -1,4 +0,0 @@
-export const standarDate = () => {
-  const dateObj = new Date();
-  return dateObj.getUTCDate() + "/" + dateObj.getUTCMonth() + 1;
-};
--- server/src/components/test_paragraph/validation.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const createParagraph = yup.object().shape({
-    text: yup.string().required().trim(),
-    image_id: yup.number().required(),
-});
