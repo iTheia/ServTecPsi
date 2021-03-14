8a2ccd789e84364f97f3da0f6756a4cf698d9471
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-03-13 22:38:17 -0600

    routes

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,24 +1,31 @@
 import React, { useContext, useEffect } from "react";
-import { Loader } from "../components/Loader";
-import { RouterComponent } from "../components/Router";
-import { AppContext } from "../providers/App";
-import { AuthContext } from "../providers/Auth";
-import { adminRoutes, authRoutes, userRoutes } from "./routes";
+import {
+  AppContext,
+  AuthContext,
+  Layout,
+  Loader,
+  RouterComponent,
+} from "../components";
+import { authRoutes, userRoutes } from "./routes";
 
 export const Router: React.FC = () => {
-    const { isLoading } = useContext(AppContext);
-    const { currentUser } = useContext(AuthContext);
+  const { isLoading } = useContext(AppContext);
+  const { currentUser } = useContext(AuthContext);
 
-    useEffect(() => {}, [currentUser]);
-    if (isLoading) {
-        return <Loader />;
-    }
-    if (currentUser === false) {
-        return <RouterComponent routes={authRoutes} />;
-    }
-    if (currentUser.role === "admin") {
-        return <RouterComponent routes={adminRoutes} />;
-    }
-
-    return <RouterComponent routes={userRoutes} />;
+  useEffect(() => {}, [currentUser]);
+  if (isLoading) {
+    return <Loader />;
+  }
+  if (!currentUser) {
+    return (
+      <Layout>
+        <RouterComponent routes={authRoutes} />
+      </Layout>
+    );
+  }
+  return (
+    <Layout>
+      <RouterComponent routes={userRoutes} />
+    </Layout>
+  );
 };
--- client/src/routes/routes.ts
+++ client/src/routes/routes.ts
@@ -1,10 +1,42 @@
 import { IRoute } from "../constants";
-import { HomePage } from "../pages/Home";
+import {
+  AssistencePage,
+  PostPage,
+  ServicesPage,
+  SingleTestPage,
+  TestsPage,
+  HomePage,
+  SinglePost,
+  CarrerPage,
+  CurriculumPage,
+  Error404Page,
+} from "../pages";
 
 export const authRoutes: IRoute[] = [
-    { path: "/", component: HomePage, exact: true },
+  { path: "/", component: HomePage, exact: true },
+  { path: "/posts", component: PostPage, exact: true },
+  { path: "/posts/cambio-de-carrera", component: CarrerPage },
+  { path: "/posts/egresados", component: CurriculumPage },
+  { path: "/posts/talleres", component: CurriculumPage },
+  { path: "/posts/habitos-de-estudio", component: CarrerPage },
+  { path: "/posts/:id", component: SinglePost },
+  { path: "/tests", component: TestsPage, exact: true },
+  { path: "/tests/:id", component: SingleTestPage },
+  { path: "/services", component: ServicesPage, exact: true },
+  { path: "*", component: Error404Page },
 ];
 
-export const userRoutes: IRoute[] = [];
-
-export const adminRoutes: IRoute[] = [];
+export const userRoutes: IRoute[] = [
+  { path: "/", component: HomePage, exact: true },
+  { path: "/posts", component: PostPage, exact: true },
+  { path: "/posts/cambio-de-carrera", component: CarrerPage },
+  { path: "/posts/egresados", component: CurriculumPage },
+  { path: "/posts/talleres", component: CurriculumPage },
+  { path: "/posts/habitos-de-estudio", component: CarrerPage },
+  { path: "/posts/:id", component: SinglePost },
+  { path: "/tests", component: TestsPage, exact: true },
+  { path: "/tests/:id", component: SingleTestPage },
+  { path: "/services", component: ServicesPage, exact: true },
+  { path: "/help", component: AssistencePage, exact: true },
+  { path: "*", component: Error404Page },
+];
