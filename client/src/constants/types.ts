886b5428e6e7e2e53103465232d70914dab261a2
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-03-13 22:34:49 -0600

    client id

--- client/src/constants/types.ts
+++ client/src/constants/types.ts
@@ -3,15 +3,35 @@ import { RouteComponentProps } from "react-router-dom";
 export type roles = "student" | "admin";
 
 export interface IUser {
-    name: string;
-    avatar: string;
-    role: roles;
+  name: string;
+  avatar: string;
+  role: roles;
 }
 
 export interface IRoute {
-    path: string | string[];
-    component:
-        | React.ComponentType<RouteComponentProps<any>>
-        | React.ComponentType<any>;
-    exact?: boolean;
+  path: string | string[];
+  component:
+    | React.ComponentType<RouteComponentProps<any>>
+    | React.ComponentType<any>;
+  exact?: boolean;
+}
+export interface ILink {
+  to: string;
+  description: string;
+  icon?: any;
+}
+
+export interface IPaginated<T> {
+  from: number;
+  to: number;
+  current_page: number;
+  prev_page: null | IPage;
+  next_page: null | IPage;
+  total: number;
+  data: T[];
+}
+
+export interface IPage {
+  page: number;
+  limit: number;
 }
--- client/src/constants/variables.ts
+++ client/src/constants/variables.ts
@@ -1,1 +1,3 @@
-export const ENDPOINT = "";
+export const ENDPOINT = "http://localhost:5000";
+export const CLIENTID =
+  "555619313308-juuatvesksqts69pvp719lnmjhs453ck.apps.googleusercontent.com";
